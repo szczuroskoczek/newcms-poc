3430e5b2201e4231aea0f1fe52f499ce43a37367
kryst <krystianb127098@outlook.com>
2024-11-07 01:27:05 +0100

    file based db for prototyping

---
+++ util/easydb.mjs
@@ -0,0 +1,12 @@
+import pages from "../db/pages.json";
+import fs from "node:fs";
+
+export default class EasyDB {
+  pages = pages;
+
+  save() {
+    for (const [key, value] of Object.entries(this.pages)) {
+      fs.writeFileSync(`../db/${key}.json`, JSON.stringify(value, null, 4));
+    }
+  }
+}
